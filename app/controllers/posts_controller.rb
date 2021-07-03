7a6a2e7de89dea31fc4d5bb602205827ca3405ec
Raven <ravenkoehler@gmail.com>
2021-07-03 15:09:43 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,10 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    byebug
     @post = Post.new(post_params)
+
+  
 
     respond_to do |format|
       if @post.save
@@ -69,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, :tag_ids => [])
     end
 end
